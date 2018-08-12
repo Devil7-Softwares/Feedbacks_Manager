6a3598f151300de4908c015e64930b156f3b6884
Devil7DK <dineshthangavel47@gmail.com>
2018-08-12 14:31:06 +0530

    Add License Header, Namespace & HideApp Property

--- Feedbacks Manager/Controls/FeedbacksList.Designer.vb
+++ Feedbacks Manager/Controls/FeedbacksList.Designer.vb
@@ -1,59 +1,61 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class FeedbacksList
-    Inherits DevExpress.XtraEditors.XtraUserControl
+﻿Namespace Controls
+    <Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+    Partial Class FeedbacksList
+        Inherits DevExpress.XtraEditors.XtraUserControl
 
-    'UserControl overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
-    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-        If disposing AndAlso components IsNot Nothing Then
-            components.Dispose()
-        End If
-        MyBase.Dispose(disposing)
-    End Sub
+        'UserControl overrides dispose to clean up the component list.
+        <System.Diagnostics.DebuggerNonUserCode()>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
 
-    'Required by the Windows Form Designer
-    Private components As System.ComponentModel.IContainer
+        'Required by the Windows Form Designer
+        Private components As System.ComponentModel.IContainer
 
-    'NOTE: The following procedure is required by the Windows Form Designer
-    'It can be modified using the Windows Form Designer.  
-    'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
-    Private Sub InitializeComponent()
-        Me.gc_Data = New DevExpress.XtraGrid.GridControl()
-        Me.gv_Data = New DevExpress.XtraGrid.Views.Grid.GridView()
-        CType(Me.gc_Data, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.gv_Data, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.SuspendLayout()
-        '
-        'gc_Data
-        '
-        Me.gc_Data.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.gc_Data.Location = New System.Drawing.Point(0, 0)
-        Me.gc_Data.MainView = Me.gv_Data
-        Me.gc_Data.Name = "gc_Data"
-        Me.gc_Data.Size = New System.Drawing.Size(412, 302)
-        Me.gc_Data.TabIndex = 0
-        Me.gc_Data.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.gv_Data})
-        '
-        'gv_Data
-        '
-        Me.gv_Data.GridControl = Me.gc_Data
-        Me.gv_Data.Name = "gv_Data"
-        Me.gv_Data.OptionsBehavior.ReadOnly = True
-        '
-        'FeedbacksList
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Controls.Add(Me.gc_Data)
-        Me.Name = "FeedbacksList"
-        Me.Size = New System.Drawing.Size(412, 302)
-        CType(Me.gc_Data, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.gv_Data, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.ResumeLayout(False)
+        'NOTE: The following procedure is required by the Windows Form Designer
+        'It can be modified using the Windows Form Designer.  
+        'Do not modify it using the code editor.
+        <System.Diagnostics.DebuggerStepThrough()>
+        Private Sub InitializeComponent()
+            Me.gc_Data = New DevExpress.XtraGrid.GridControl()
+            Me.gv_Data = New DevExpress.XtraGrid.Views.Grid.GridView()
+            CType(Me.gc_Data, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.gv_Data, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            '
+            'gc_Data
+            '
+            Me.gc_Data.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.gc_Data.Location = New System.Drawing.Point(0, 0)
+            Me.gc_Data.MainView = Me.gv_Data
+            Me.gc_Data.Name = "gc_Data"
+            Me.gc_Data.Size = New System.Drawing.Size(412, 302)
+            Me.gc_Data.TabIndex = 0
+            Me.gc_Data.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.gv_Data})
+            '
+            'gv_Data
+            '
+            Me.gv_Data.GridControl = Me.gc_Data
+            Me.gv_Data.Name = "gv_Data"
+            Me.gv_Data.OptionsBehavior.ReadOnly = True
+            '
+            'FeedbacksList
+            '
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.Controls.Add(Me.gc_Data)
+            Me.Name = "FeedbacksList"
+            Me.Size = New System.Drawing.Size(412, 302)
+            CType(Me.gc_Data, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.gv_Data, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
 
-    End Sub
+        End Sub
 
-    Friend WithEvents gc_Data As DevExpress.XtraGrid.GridControl
-    Friend WithEvents gv_Data As DevExpress.XtraGrid.Views.Grid.GridView
-End Class
+        Friend WithEvents gc_Data As DevExpress.XtraGrid.GridControl
+        Friend WithEvents gv_Data As DevExpress.XtraGrid.Views.Grid.GridView
+    End Class
+End Namespace
--- Feedbacks Manager/Controls/FeedbacksList.vb
+++ Feedbacks Manager/Controls/FeedbacksList.vb
@@ -1,33 +1,81 @@
-﻿Public Class FeedbacksList
+﻿'=========================================================================='
+'                                                                          '
+'                    (C) Copyright 2018 Devil7 Softwares.                  '
+'                                                                          '
+' Licensed under the Apache License, Version 2.0 (the "License");          '
+' you may not use this file except in compliance with the License.         '
+' You may obtain a copy of the License at                                  '
+'                                                                          '
+'                http://www.apache.org/licenses/LICENSE-2.0                '
+'                                                                          '
+' Unless required by applicable law or agreed to in writing, software      '
+' distributed under the License is distributed on an "AS IS" BASIS,        '
+' WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. '
+' See the License for the specific language governing permissions and      '
+' limitations under the License.                                           '
+'                                                                          '
+' Contributors :                                                           '
+'     Dineshkumar T                                                        '
+'                                                                          '
+'=========================================================================='
 
-    Property List As List(Of Classes.Feedback)
-        Get
-            If gc_Data.DataSource Is Nothing Then
-                gc_Data.DataSource = New List(Of Classes.Feedback)
-            End If
-            Return gc_Data.DataSource
-        End Get
-        Set(value As List(Of Classes.Feedback))
-            gc_Data.DataSource = value
-        End Set
-    End Property
+Namespace Controls
+    Public Class FeedbacksList
 
-    ReadOnly Property SelectedItemsCount As Integer
-        Get
-            Return gv_Data.SelectedRowsCount
-        End Get
-    End Property
+        Property List As List(Of Classes.Feedback)
+            Get
+                If gc_Data.DataSource Is Nothing Then
+                    gc_Data.DataSource = New List(Of Classes.Feedback)
+                End If
+                Return gc_Data.DataSource
+            End Get
+            Set(value As List(Of Classes.Feedback))
+                gc_Data.DataSource = value
+                gc_Data.RefreshDataSource()
 
-    Function GetSelectedItems() As List(Of Classes.Feedback)
-        Dim R As New List(Of Classes.Feedback)
+                If HideAppName Then
+                    gv_Data.Columns("AppName").Visible = False
+                Else
+                    gv_Data.Columns("AppName").Visible = True
+                End If
+            End Set
+        End Property
 
-        For Each i As Integer In gv_Data.GetSelectedRows
-            R.Add(gv_Data.GetRow(i))
-        Next
+        ReadOnly Property SelectedItemsCount As Integer
+            Get
+                Return gv_Data.SelectedRowsCount
+            End Get
+        End Property
 
-        Return R
-    End Function
+        Function GetSelectedItems() As List(Of Classes.Feedback)
+            Dim R As New List(Of Classes.Feedback)
 
+            For Each i As Integer In gv_Data.GetSelectedRows
+                R.Add(gv_Data.GetRow(i))
+            Next
 
+            Return R
+        End Function
 
-End Class
+        Dim HideAppName_ As Boolean
+        Property HideAppName As Boolean
+            Get
+                Return HideAppName_
+            End Get
+            Set(value As Boolean)
+                HideAppName_ = value
+
+                Try
+                    If value Then
+                        gv_Data.Columns("AppName").Visible = False
+                    Else
+                        gv_Data.Columns("AppName").Visible = True
+                    End If
+                Catch ex As Exception
+
+                End Try
+            End Set
+        End Property
+
+    End Class
+End Namespace
